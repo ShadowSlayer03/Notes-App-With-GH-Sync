5fadf9f9d5576b00a0c55c41fe130455790787d7
ShadowSlayer03 <sslayer441@gmail.com>
2026-08-30 12:28:08 +0530

    Added constants for overall app

---
+++ frontend/src/lib/constants/app.ts
@@ -0,0 +1,15 @@
+
+const appName = "Kairno";
+
+const appUrl = "https://kairno.com";
+
+const mainTitle = "Kairno | Versioned Notes That Stay Yours";
+
+const mainDesc = "Beautiful markdown notes with Git-powered version history. Own your notes forever, sync through GitHub."
+
+export {
+    appName,
+    appUrl,
+    mainTitle,
+    mainDesc
+}
