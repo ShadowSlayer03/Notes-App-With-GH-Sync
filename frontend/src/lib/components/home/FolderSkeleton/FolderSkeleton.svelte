5a5f4adae10622e327c72f98bd70cae9f2dfa0ec
ShadowSlayer03 <sslayer441@gmail.com>
2026-09-05 14:57:51 +0530

    Added skeleton loader for folders in home page

---
+++ frontend/src/lib/components/home/FolderSkeleton/FolderSkeleton.svelte
@@ -0,0 +1,26 @@
+<div class="flex w-[200px] shrink-0 flex-col gap-3 animate-pulse">
+	<div class="relative h-[190px] w-full rounded-2xl border border-zinc-800 bg-[#14171d]">
+		<div class="absolute left-0 top-0 bottom-0 w-2.5 rounded-l-2xl bg-zinc-800"></div>
+
+		<div class="absolute right-4 top-0 bottom-[-6px] z-10 w-[14px] rounded-b-sm bg-zinc-800"></div>
+
+		<div class="flex h-full flex-col justify-between p-4">
+			<div class="mt-1 flex gap-1">
+				<div class="h-[3px] w-[3px] rounded-full bg-zinc-700"></div>
+				<div class="h-[3px] w-[3px] rounded-full bg-zinc-700"></div>
+				<div class="h-[3px] w-[3px] rounded-full bg-zinc-700"></div>
+			</div>
+
+			<div class="flex justify-center">
+				<div class="h-8 w-8 rounded-full bg-zinc-700"></div>
+			</div>
+
+			<div class="h-3 w-16 rounded bg-zinc-700"></div>
+		</div>
+	</div>
+
+	<div class="space-y-2 px-1">
+		<div class="h-4 w-28 rounded bg-zinc-700"></div>
+		<div class="h-3 w-36 rounded bg-zinc-800"></div>
+	</div>
+</div>
