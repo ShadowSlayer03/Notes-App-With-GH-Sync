0f39a52e2958c62fed9015c9b758dbf7b012889b
ShadowSlayer03 <sslayer441@gmail.com>
2026-09-14 23:20:21 +0530

    Add constants for repository configuration and metadata

---
+++ backend/src/github/constants.ts
@@ -0,0 +1,15 @@
+
+const repoName = 'Kairno-Notes';
+
+const metadataFileName = '.kairno.json';
+
+const allNotesFolderId = 'all-notes';
+
+const defaultNoteTheme = 'default';
+
+export {
+    repoName,
+    metadataFileName,
+    allNotesFolderId,
+    defaultNoteTheme
+}
