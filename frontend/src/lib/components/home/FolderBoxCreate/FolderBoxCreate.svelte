304f6aeb8ec342e50ff98f78a5b242cd948569ee
ShadowSlayer03 <sslayer441@gmail.com>
2026-09-18 20:47:00 +0530

    Add data-testid attributes to FolderBoxCreate for improved testing support

--- frontend/src/lib/components/home/FolderBoxCreate/FolderBoxCreate.svelte
+++ frontend/src/lib/components/home/FolderBoxCreate/FolderBoxCreate.svelte
@@ -20,14 +20,17 @@
 <button
 	class="flex flex-col items-center justify-center border border-dotted border-zinc-800/90 bg-[#12141c]/20 rounded-2xl p-5 hover:border-zinc-700 hover:bg-zinc-800/10 transition-all cursor-pointer group w-[200px] h-[190px] shrink-0"
 	onclick={createNewFolder}
+	data-testid="create-new-folder-button"
 >
 	<div
 		class="p-2 rounded-xl bg-zinc-900 border border-zinc-800 text-zinc-500 group-hover:text-zinc-300 transition-all"
 	>
-		<Plus class="h-5 w-5" />
+		<Plus class="h-5 w-5" data-testid="plus-icon" />
 	</div>
 	<span
 		class="text-xs font-medium text-zinc-500 mt-3 group-hover:text-zinc-400 text-center leading-tight"
-		>Create new<br />folder</span
+		data-testid="create-new-folder-text"
 	>
+		Create new<br />folder
+	</span>
 </button>
