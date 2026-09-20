434ddd27284d6d94d6fd440abd3b29b8742deeed
ShadowSlayer03 <sslayer441@gmail.com>
2026-09-20 11:23:02 +0530

    Added RecentNotesQueryWrapper for tests for QueryProvider

---
+++ frontend/src/lib/test/wrappers/RecentNotesQueryWrapper.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import RecentNotes from '$lib/components/home/RecentNotes/RecentNotes.svelte';
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				retry: false
+			},
+			mutations: {
+				retry: false
+			}
+		}
+	});
+</script>
+
+<QueryClientProvider client={queryClient}>
+	<RecentNotes />
+</QueryClientProvider>
