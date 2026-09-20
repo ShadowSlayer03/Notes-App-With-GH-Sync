32cf8d36759564daf040101c1586833af8ae3e0e
ShadowSlayer03 <sslayer441@gmail.com>
2026-09-20 11:22:48 +0530

    Added FolderBoxCreateQueryWrapper for tests for QueryProvider

---
+++ frontend/src/lib/test/wrappers/FolderBoxCreateQueryWrapper.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+
+	import FolderBoxCreate from '$lib/components/home/FolderBoxCreate/FolderBoxCreate.svelte';
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				retry: false
+			},
+			mutations: {
+				retry: false
+			}
+		}
+	});
+</script>
+
+<QueryClientProvider client={queryClient}>
+	<FolderBoxCreate />
+</QueryClientProvider>
